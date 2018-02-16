cf02db5e1c9e9626aa29065418f5664c4a97699e
jjayp4rk <jpark0227@gmail.com>
2018-02-16 01:23:29 -0500

    Request URL

--- app.js
+++ app.js
@@ -9,13 +9,17 @@ var port = process.env.port || 3000;
 
 app.use(express.static('public'));
 
+app.use('/', function (req, res, next){
+    console.log('Request URL: ' + req.url);
+    next();
+});
 // Home route
 app.get('/', function(req, res){
   res.sendFile(path.join(__dirname +'/index.html'));
 });
 // API/Greeting route
 app.get('/api/greeting', function(req,res) {
-  res.json({name: 'Jay', last name: 'Park'});
+  res.json({name: 'Jay', lastname: 'Park'});
 });
 
 // Start Server
